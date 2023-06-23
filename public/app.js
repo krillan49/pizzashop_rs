62b13591d4ae60289b306f987e5cac758e5c0259
krillan <pushkinidze49@gmail.com>
2023-06-23 08:27:28 +0300

    добавлены js-функции для счетчиков товаров и кнопки отправки, изменены стили кнопок и добавлена кнопка отправки

--- public/app.js
+++ public/app.js
@@ -1,8 +1,51 @@
 // Счетчик отдельных покупок для корзины
 function addToCart(id) {
     var key = 'product_' + id;
-    
+
     var x = window.localStorage.getItem(key);
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
+
+    updateOrdersInput();
+    updateOrdersButton();
+}
+
+// Функция для того чтобы обратиться к полю в которое мы хотим поместить "product_1=3,product_2=5,product_3=1, ..." чтобы потом отправить этот результат на сервер.
+function updateOrdersInput() {
+    var orders = сartGetOrders();
+    $('#orders_input').val(orders);    
+}
+
+// Функция для того чтобы обратиться к кнопке в имя которой мы хотим поместить общее число заказов чтобы информацию о заказах было видно клиенту.
+function updateOrdersButton() {
+    var text = 'Корзина (' + cartGetNumberOfItems() + ' шт.)';
+    $('#orders_button').val(text);
+}
+
+// Счетчик для общего количества товаров в корзине:
+function cartGetNumberOfItems() {
+    var cnt = 0;
+    for (var i = 0; i < window.localStorage.length; i++){
+        var key = window.localStorage.key(i); // получаем ключ
+        var value = window.localStorage.getItem(key); // получаем значение
+
+        if(key.indexOf('product_') == 0) {
+            cnt = cnt + value * 1;
+        }
+    }
+    return cnt;
+}
+
+// Функция для получения(и дальнейшей отправки через форму на сервер) результата о заказах из localStorage вида "product_1=3,product_2=5,product_3=1, ..."
+function сartGetOrders() {
+    var orders = '';
+    for (var i = 0; i < window.localStorage.length; i++){
+        var key = window.localStorage.key(i); // получаем ключ
+        var value = window.localStorage.getItem(key); // получаем значение
+
+        if(key.indexOf('product_') == 0) {
+            orders = orders + key + '=' + value + ','; //  "product_1=3,"
+        }
+    }
+    return orders; // "product_1=3,product_2=5,product_3=1, ..."
 }
